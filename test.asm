6405e78b8c8c8291e8e2e85046c1ac7581879638
Dimitri Bobkov <bobkov.dimitri@gmail.com>
2021-09-04 14:14:59 +0100

    none

--- test.asm
+++ test.asm
@@ -6,14 +6,21 @@ main:
 
     move r_b, r_a
 
-    load f_r_a, 0x4079999a
 
-    move f_r_a, r_c
+
+    load r_c, 0xFFED
 
     .loop:
         jmp add_nums
+        sw 0xFFED, r_a
         ifn r_a, 250*5000, .loop
 
+    lw r_a, 0xFFED
+
+    load f_r_a, 0x4079999a
+
+    move f_r_a, r_c
+    
     hlt
 
 
